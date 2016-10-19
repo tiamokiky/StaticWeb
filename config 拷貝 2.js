be8372f7d0d07a8dc5079f78fc08858a5119ab16
tiamokiky <tiamokiky@gmial.com>
2016-10-19 15:41:58 +0800

    Add my files

---
+++ config 拷貝 2.js
@@ -0,0 +1,22 @@
+window.widgetConfig = [
+  {
+    widgetName: 'News and Information',
+    source: 'widget-notification',
+    widgetId: 1,
+    protocol: 'cms.client.ah7l6xejee',
+    header: 'News and Information',
+    groupId: 'ah7l6xejee',
+    serverURL: 'https://apisx.jay.bo:3001/dev',
+    apiKey: '7322226d2558797c55333f7079315d4b4c4759474a5e227c4a2b62422e'
+  },
+  {
+    widgetName: 'Health and Wellness',
+    source: 'widget-notification',
+    widgetId: 2,
+    protocol: 'cms.client.hWBx1dwuoT',
+    header: 'Health and Wellness',
+    groupId: 'hWBx1dwuoT',
+    serverURL: 'https://apisx.jay.bo:3001/dev',
+    apiKey: '7322226d2558797c55333f7079315d4b4c4759474a5e227c4a2b62422e',
+  }
+]
---
+++ config.js
@@ -0,0 +1,22 @@
+window.widgetConfig = [
+  {
+    widgetName: 'News and Information',
+    source: 'widget-notification',
+    widgetId: 1,
+    protocol: 'cms.client.pjk1UnMbKV',
+    header: 'News and Information',
+    groupId: 'pjk1UnMbKV',
+    serverURL: 'https://api-jaybo.parseapp.com',
+    apiKey: '6d7a5433345f37672c32424c3159392a222d372b506d6945794e47355a'
+  },
+  {
+    widgetName: 'Health and Wellness',
+    source: 'widget-notification',
+    widgetId: 2,
+    protocol: 'cms.client.hWBx1dwuoT',
+    header: 'Health and Wellness',
+    groupId: 'hWBx1dwuoT',
+    serverURL: 'https://api-jaybo.parseapp.com',
+    apiKey: '6d7a5433345f37672c32424c3159392a222d372b506d6945794e47355a',
+  }
+]
